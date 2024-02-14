048e1f7b5a39a1403998c5911b26caa593388ac2
asifner <a.sifner123@gmail.com>
2024-02-14 11:40:15 +0100

    Adding entities for database.

---
+++ WebApplication2/WebApplication2/Entities/Order.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="Order.cs" company="Danieli Systec d.o.o.">
+// Copyright (c) Danieli Systec d.o.o.. All rights reserved.
+// CONFIDENTIAL; Property of Danieli Systec d.o.o.
+// Unauthorized reproduction, copying, distribution or any other use of the whole or any part of this documentation/data/software is strictly prohibited.
+// </copyright>
+
+namespace WebApplication2.Entities
+{
+    /// <summary>
+    /// Represents an order entity.
+    /// </summary>
+    public class Order
+    {
+        /// <summary>Gets or sets the identifier.</summary>
+        /// <value>The identifier.</value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date of order.
+        /// </summary>
+        /// <value>
+        /// The date of order.
+        /// </value>
+        public DateTime DateOfOrder { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="Order"/> is status.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if status; otherwise, <c>false</c>.
+        /// </value>
+        public bool Status { get; set; }
+    }
+}
---
+++ WebApplication2/WebApplication2/Entities/User.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="User.cs" company="Danieli Systec d.o.o.">
+// Copyright (c) Danieli Systec d.o.o.. All rights reserved.
+// CONFIDENTIAL; Property of Danieli Systec d.o.o.
+// Unauthorized reproduction, copying, distribution or any other use of the whole or any part of this documentation/data/software is strictly prohibited.
+// </copyright>
+
+namespace WebApplication2.Entities
+{
+    /// <summary>
+    /// Represents a user entity.
+    /// </summary>
+    public class User
+    {
+        /// <summary>Gets or sets the identifier.</summary>
+        /// <value>The identifier.</value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public required string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        /// <value>
+        /// The last name.
+        /// </value>
+        public required string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the telephone.
+        /// </summary>
+        /// <value>
+        /// The telephone.
+        /// </value>
+        public required string Telephone { get; set; }
+    }
+}
