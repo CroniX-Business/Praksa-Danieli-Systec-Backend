6dca745bb65848bc0f67b5e58652aba8dbd3497c
robertklanjscek <robert.klanjscek123@gmail.com>
2024-02-14 14:35:22 +0100

    Adding new controllers for entities

---
+++ WebApplication2/WebApplication2/Controllers/RestaurantController.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="RestaurantController.cs" company="Danieli Systec d.o.o.">
+// Copyright (c) Danieli Systec d.o.o.. All rights reserved.
+// CONFIDENTIAL; Property of Danieli Systec d.o.o.
+// Unauthorized reproduction, copying, distribution or any other use of the whole or any part of this documentation/data/software is strictly prohibited.
+// </copyright>
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Entities;
+
+namespace WebApplication2.Controllers
+{
+    /// <summary>
+    ///   Controller for restaurant to return data.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantController : ControllerBase
+    {
+        /// <summary>Gets the restaurant data.</summary>
+        /// <returns>
+        ///   Returns data of all restaurants.
+        /// </returns>
+        [HttpGet]
+        public ActionResult<List<Restaurant>> GetAllRestaurants()
+        {
+            var restaurant = new List<Restaurant>
+            {
+                new Restaurant
+                {
+                    Id = 1,
+                    Name = "Test",
+                    Address = "kralja mazuranica 21",
+                    TelephoneNumber = "1234567890",
+                },
+            };
+            return this.Ok(restaurant);
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="UserController.cs" company="Danieli Systec d.o.o.">
+// Copyright (c) Danieli Systec d.o.o.. All rights reserved.
+// CONFIDENTIAL; Property of Danieli Systec d.o.o.
+// Unauthorized reproduction, copying, distribution or any other use of the whole or any part of this documentation/data/software is strictly prohibited.
+// </copyright>
+
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Entities;
+
+namespace WebApplication2.Controllers
+{
+    /// <summary>
+    ///   Represents a user controller.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        /// <summary>Gets all users.</summary>
+        /// <returns>
+        ///   Returns list of users.
+        /// </returns>
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            var users = new List<User>()
+            {
+                new User()
+                {
+                    Id = 1,
+                    Name = "Test",
+                    LastName = "Test",
+                    Telephone = "123456789",
+                },
+            };
+
+            return this.Ok(users);
+        }
+    }
+}
