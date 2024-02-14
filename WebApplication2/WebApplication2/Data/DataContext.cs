0cf679760b3020d3aca49839b213da757916ccd5
asifner <a.sifner123@gmail.com>
2024-02-14 14:47:06 +0100

    EntityFrameworkCore Connection.

--- WebApplication2/WebApplication2/Data/DataContext.cs
+++ WebApplication2/WebApplication2/Data/DataContext.cs
@@ -1,6 +1,21 @@
-﻿namespace WebApplication2.Data
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Entities;
+
+namespace WebApplication2.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Item> Items { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
