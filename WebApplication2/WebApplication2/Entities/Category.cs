4df6bc6985f3ea68937cc6e4aed61d8f7d1bfb5a
MarioPanduric <mariopanduric123@hotmail.com>
2024-02-14 11:39:21 +0100

    Created entities for database

---
+++ WebApplication2/WebApplication2/Entities/Category.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="Category.cs" company="Danieli Systec d.o.o.">
+// Copyright (c) Danieli Systec d.o.o.. All rights reserved.
+// CONFIDENTIAL; Property of Danieli Systec d.o.o.
+// Unauthorized reproduction, copying, distribution or any other use of the whole or any part of this documentation/data/software is strictly prohibited.
+// </copyright>
+
+namespace WebApplication2.Entities
+{
+    /// <summary>
+    /// Entity for Category.
+    /// </summary>
+    public class Category
+    {
+        /// <summary>Gets or sets the identifier for ID.</summary>
+        /// <value>The identifier.</value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public required string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sort.
+        /// </summary>
+        /// <value>
+        /// The sort.
+        /// </value>
+        public int Sort { get; set; }
+
+        /// <summary>
+        /// Gets or sets the restaurant identifier.
+        /// </summary>
+        /// <value>
+        /// The restaurant identifier.
+        /// </value>
+        public required Restaurant RestaurantId { get; set; }
+    }
+}
---
+++ WebApplication2/WebApplication2/Entities/OrderItem.cs
@@ -0,0 +1,66 @@
+﻿// <copyright file="OrderItem.cs" company="Danieli Systec d.o.o.">
+// Copyright (c) Danieli Systec d.o.o.. All rights reserved.
+// CONFIDENTIAL; Property of Danieli Systec d.o.o.
+// Unauthorized reproduction, copying, distribution or any other use of the whole or any part of this documentation/data/software is strictly prohibited.
+// </copyright>
+
+namespace WebApplication2.Entities
+{
+    /// <summary>
+    /// Entity for order item.
+    /// </summary>
+    public class OrderItem
+    {
+        /// <summary>Gets or sets the identifier for ID.</summary>
+        /// <value>The identifier.</value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public required string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the price.
+        /// </summary>
+        /// <value>
+        /// The price.
+        /// </value>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Gets or sets the category identifier.
+        /// </summary>
+        /// <value>
+        /// The category identifier.
+        /// </value>
+        public required Category CategoryId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the quantity.
+        /// </summary>
+        /// <value>
+        /// The quantity.
+        /// </value>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the order identifier.
+        /// </summary>
+        /// <value>
+        /// The order identifier.
+        /// </value>
+        public required Order OrderId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user identifier.
+        /// </summary>
+        /// <value>
+        /// The user identifier.
+        /// </value>
+        public required User UserId { get; set; }
+    }
+}
