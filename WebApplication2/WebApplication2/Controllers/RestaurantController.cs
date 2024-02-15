114e46580942a6ec9575924831701e06169809a2
robertklanjscek <robert.klanjscek123@gmail.com>
2024-02-15 14:40:25 +0100

    implemented CRUD for easier controlling database.

--- WebApplication2/WebApplication2/Controllers/RestaurantController.cs
+++ WebApplication2/WebApplication2/Controllers/RestaurantController.cs
@@ -6,6 +6,8 @@
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data;
 using WebApplication2.Entities;
 
 namespace WebApplication2.Controllers
@@ -13,28 +15,96 @@ namespace WebApplication2.Controllers
     /// <summary>
     ///   Controller for restaurant to return data.
     /// </summary>
+    /// <remarks>Initializes a new instance of the <see cref="RestaurantController" /> class.</remarks>
+    /// <param name="context">The context.</param>
     [Route("api/[controller]")]
     [ApiController]
-    public class RestaurantController : ControllerBase
+    public class RestaurantController(DataContext context) : ControllerBase
     {
+        /// <summary>The context.</summary>
+        private readonly DataContext context = context;
+
         /// <summary>Gets the restaurant data.</summary>
-        /// <returns>
-        ///   Returns data of all restaurants.
-        /// </returns>
+        /// <returns>Returns data of all restaurants.</returns>
         [HttpGet]
-        public ActionResult<List<Restaurant>> GetAllRestaurants()
+        public async Task<ActionResult<List<Restaurant>>> GetAllRestaurants()
         {
-            var restaurant = new List<Restaurant>
-            {
-                new()
-                {
-                    Id = 1,
-                    Name = "Test",
-                    Address = "kralja mazuranica 21",
-                    TelephoneNumber = "1234567890",
-                },
-            };
+            var restaurant = await this.context.Restaurants.ToListAsync();
             return this.Ok(restaurant);
+        }
+
+        /// <summary>Gets the restaurant.</summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>
+        ///  Returns restaurant by id.
+        /// </returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Restaurant>> GetRestaurant(int id)
+        {
+            var restaurant = await this.context.Restaurants.FindAsync(id);
+            if (restaurant is null)
+            {
+                return this.NotFound("Restaurant not found");
+            }
+
+            return this.Ok(restaurant);
+        }
+
+        /// <summary>Adds the restaurant.</summary>
+        /// <param name="restaurant">The restaurant.</param>
+        /// <returns>
+        ///  Posts restaurant to database.
+        /// </returns>
+        [HttpPost]
+        public async Task<ActionResult<List<Restaurant>>> AddRestaurant(Restaurant restaurant)
+        {
+            this.context.Restaurants.Add(restaurant);
+            await this.context.SaveChangesAsync();
+
+            return this.Ok(await this.context.Restaurants.ToListAsync());
+        }
+
+        /// <summary>Updates the restaurant.</summary>
+        /// <param name="updatedRestaurant">The updated restaurant.</param>
+        /// <returns>
+        ///  Updates parameters of restaurant.
+        /// </returns>
+        [HttpPut]
+        public async Task<ActionResult<List<Restaurant>>> UpdateRestaurant(Restaurant updatedRestaurant)
+        {
+            var dbRestaurant = await this.context.Restaurants.FindAsync(updatedRestaurant.Id);
+            if (dbRestaurant is null)
+            {
+                return this.NotFound("Restaurant not found");
+            }
+
+            dbRestaurant.Name = updatedRestaurant.Name;
+            dbRestaurant.Address = updatedRestaurant.Address;
+            dbRestaurant.TelephoneNumber = updatedRestaurant.TelephoneNumber;
+
+            await this.context.SaveChangesAsync();
+
+            return this.Ok(await this.context.Restaurants.ToListAsync());
+        }
+
+        /// <summary>Deletes the restaurant.</summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>
+        ///  Deletes restaurant by id.
+        /// </returns>
+        [HttpDelete]
+        public async Task<ActionResult<List<Restaurant>>> DeleteRestaurant(int id)
+        {
+            var dbRestaurant = await this.context.Restaurants.FindAsync(id);
+            if (dbRestaurant is null)
+            {
+                return this.NotFound("Restaurant not found");
+            }
+
+            this.context.Restaurants.Remove(dbRestaurant);
+            await this.context.SaveChangesAsync();
+
+            return this.Ok(await this.context.Restaurants.ToListAsync());
         }
     }
 }
--- WebApplication2/WebApplication2/Migrations/20240215121618_UpdatingRelations.cs
+++ WebApplication2/WebApplication2/Migrations/20240215121618_UpdatingRelations.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿// <copyright file="20240215121618_UpdatingRelations.cs" company="Danieli Systec d.o.o.">
+// Copyright (c) Danieli Systec d.o.o.. All rights reserved.
+// CONFIDENTIAL; Property of Danieli Systec d.o.o.
+// Unauthorized reproduction, copying, distribution or any other use of the whole or any part of this documentation/data/software is strictly prohibited.
+// </copyright>
+
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
