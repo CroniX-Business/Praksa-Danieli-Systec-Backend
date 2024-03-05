133f81cfbeae38ab1bbb6e29fc76cba1354a860c
MarioPanduric <mariopanduric123@hotmail.com>
2024-03-05 14:47:48 +0100

    Added unit tests for get and post methods for RestaurantController

---
+++ OrdersApi/OrdersUnitTests/ControllerTest/RestaurantControllerTests.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OrdersApi.Controllers;
+using OrdersApi.Data;
+using OrdersApi.Dto;
+
+namespace OrdersUnitTests.ControllerTest
+{
+    public class RestaurantControllerTests
+    {
+        private readonly DataContext context;
+        private readonly IMapper mapper;
+        private readonly ILogger<RestaurantController> logger;
+
+        [Fact]
+        public void GetAllTest()
+        {
+            var controller = new RestaurantController(context, mapper, logger);
+
+            var result = controller.GetAllRestaurants();
+
+            result.Should().BeOfType<Task<ActionResult<IEnumerable<RestaurantDto>>>>();
+        }
+        [Fact]
+        public void GetRestaurantTest()
+        {
+            var controller = new RestaurantController(context, mapper, logger);
+            
+            var result = controller.GetRestaurant(1);
+
+            result.Should().BeOfType<Task<ActionResult<RestaurantDto>>>();
+        }
+
+        [Fact]
+        public void AddRestaurantTest() 
+        {
+            var controller = new RestaurantController(context, mapper, logger);
+
+            var restaurant = new RestaurantDto
+            {
+                Name = "TestRestaurant",
+                Address = "TestAddress",
+                PhoneNumber = "123456",
+            };
+
+            var result = controller.AddRestaurant(restaurant);
+
+            result.Should().BeOfType<Task<ActionResult<RestaurantDto>>>();
+
+        }
+    }
+}
