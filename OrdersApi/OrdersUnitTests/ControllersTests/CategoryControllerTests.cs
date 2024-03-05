ea8981ba802c0eb64f2177724863cb44b1a0e2c6
asifner <a.sifner123@gmail.com>
2024-03-05 14:46:11 +0100

    Unit tests.

---
+++ OrdersApi/OrdersUnitTests/ControllersTests/CategoryControllerTests.cs
@@ -0,0 +1,114 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OrdersApi.Controllers;
+using OrdersApi.Data;
+using OrdersApi.Dto;
+using OrdersApi.Entities;
+
+namespace OrdersUnitTests.ControllersTests
+{
+    public class CategoryControllerTests
+    {
+        private readonly DataContext context;
+        private readonly IMapper mapper;
+        private readonly ILogger<CategoryController> logger;
+
+        [Fact]
+        public void CategoryController_GetAllCategories_ReturnsOk()
+        {
+            //Arrange
+            var categoryController = new CategoryController(context, mapper, logger);
+
+            //Act
+            var result = categoryController.GetAllCategories();
+
+            //Assert
+            result.Should().BeOfType<Task<ActionResult<IEnumerable<CategoryDto>>>>();
+        }
+
+
+        [Fact]
+        public void CategoryController_GetCategory_ReturnsOk()
+        {
+            //Arrange
+            var categoryController = new CategoryController(context, mapper, logger);
+            var category = new Category()
+            {
+                Id = 1,
+                Name = "Test",
+                Sort = 1,
+            };
+
+            //Act
+            var result = categoryController.GetCategory(category.Id);
+
+            //Assert
+            result.Should().BeOfType<Task<ActionResult<CategoryDto>>>();
+        }
+
+        [Fact]
+        public void CategoryController_AddCategory_ReturnsCreatedAtAction()
+        {
+            //Arrange
+            var categoryController = new CategoryController(context, mapper, logger);
+            var categoryDto = new CategoryDto()
+            {
+                Id = 1,
+                Name = "Test",
+                Sort = 1,
+            };
+
+            //Act
+            var result = categoryController.AddCategory(categoryDto);
+
+            //Assert
+            result.Should().BeOfType<Task<ActionResult<CategoryDto>>>();
+        }
+
+        [Fact]
+        public void CategoryController_UpdateCategory_ReturnsNoContent()
+        {
+            //Arrange
+            var categoryController = new CategoryController(context, mapper, logger);
+            var category = new Category()
+            {
+                Id = 1,
+                Name = "Test",
+                Sort = 1,
+            };
+            var categoryDto = new CategoryDto()
+            {
+                Id = 1,
+                Name = "Test",
+                Sort = 1,
+            };
+
+            //Act
+            var result = categoryController.UpdateCategory(category.Id, categoryDto);
+
+            //Assert
+            result.Should().BeOfType<Task<ActionResult<CategoryDto>>>();
+        }
+
+        [Fact]
+        public void CategoryController_DeleteCategory_ReturnsNoContent()
+        {
+            //Arrange
+            var categoryController = new CategoryController(context, mapper, logger);
+            var category = new Category()
+            {
+                Id = 1,
+                Name = "Test",
+                Sort = 1,
+            };
+
+            //Act
+            var result = categoryController.DeleteCategory(category.Id);
+
+            //Assert
+            result.Should().BeOfType<Task<ActionResult>>();
+        }
+    }
+}
---
+++ OrdersApi/OrdersUnitTests/ControllersTests/RestaurantControllerTests.cs
@@ -0,0 +1,118 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OrdersApi.Controllers;
+using OrdersApi.Data;
+using OrdersApi.Dto;
+using OrdersApi.Entities;
+
+namespace OrdersUnitTests.ControllersTests
+{
+    public class RestaurantControllerTests
+    {
+        private readonly DataContext context;
+        private readonly IMapper mapper;
+        private readonly ILogger<RestaurantController> logger;
+
+        [Fact]
+        public void RestaurantController_GetAllRestaurants_ReturnsOk()
+        {
+            //Arrange
+            var restaurantController = new RestaurantController(context, mapper, logger);
+
+            //Act
+            var result = restaurantController.GetAllRestaurants();
+            
+            //Assert
+            result.Should().BeOfType<Task<ActionResult<IEnumerable<RestaurantDto>>>>();
+        }
+
+        [Fact]
+        public void RestaurantController_GetRestaurant_ReturnsOk()
+        {
+            //Arrange
+            var restaurantController = new RestaurantController(context, mapper, logger);
+            var restaurant = new Restaurant()
+            {
+                Id = 1,
+                Address = "Test",
+                Name = "Test",
+                PhoneNumber = "32131321",
+            };
+
+            //Act
+            var result = restaurantController.GetRestaurant(restaurant.Id);
+
+            //Assert
+            result.Should().BeOfType<Task<ActionResult<RestaurantDto>>>();
+        }
+
+        [Fact]
+        public void RestaurantController_AddRestaurant_ReturnsCreatedAtAction()
+        {
+            //Arrange
+            var restaurantController = new RestaurantController(context, mapper, logger);
+            var restaurantDto = new RestaurantDto()
+            {
+                Id = 1,
+                Address = "Test",
+                Name = "Test",
+                PhoneNumber = "12321312312",
+            };
+
+            //Act
+            var result = restaurantController.AddRestaurant(restaurantDto);
+
+            //Assert
+            result.Should().BeOfType<Task<ActionResult<RestaurantDto>>>();
+        }
+
+        [Fact]
+        public void RestaurantController_UpdateRestaurant_ReturnsNoContent()
+        {
+            //Arrange
+            var restaurantController = new RestaurantController(context, mapper, logger);
+            var restaurant = new Restaurant()
+            {
+                Id = 1,
+                Address = "Test",
+                Name = "Test",
+                PhoneNumber = "32131321",
+            };
+            var restaurantDto = new RestaurantDto()
+            {
+                Id = 1,
+                Address = "Test",
+                Name = "Test",
+                PhoneNumber = "12321312312",
+            };
+
+            //Act
+            var result = restaurantController.UpdateRestaurant(restaurant.Id, restaurantDto);
+
+            //Assert
+            result.Should().BeOfType<Task<ActionResult<RestaurantDto>>>();
+        }
+
+        [Fact]
+        public void RestaurantController_DeleteRestaurant_ReturnsNoContent()
+        {
+            //Arrange
+            var restaurantController = new RestaurantController(context, mapper, logger);
+            var restaurant = new Restaurant()
+            {
+                Id = 1,
+                Address = "Test",
+                Name = "Test",
+                PhoneNumber = "32131321",
+            };
+
+            //Act
+            var result = restaurantController.DeleteRestaurant(restaurant.Id);
+
+            //Assert
+            result.Should().BeOfType<Task<ActionResult>>();
+        }
+    }
+}
